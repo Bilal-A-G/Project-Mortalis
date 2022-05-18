b98903b46772ab4e43ff45ef889a48cad621f062
Bilal Abdul Gaffoor <bilalabdulg@gmail.com>
2022-05-18 17:27:09 -0400

    Removed the crosshair and added bullet tracers

---
+++ Assets/Scripts/Entities/Actions/Animations/LerpAndDestroyObject.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Lerp And Destroy", menuName = "FSM/Actions/Lerp And Destroy")]
+public class LerpAndDestroyObject : ActionBase
+{
+    public EventObject onLerpEnd;
+
+    public GenericReference<Vector3> endVector;
+
+    public GameObject lerpObject;
+    public GenericReference<Path> pathToInstantiatePosition;
+
+    public GenericReference<float> currentSpeed;
+
+    public GenericReference<float> tolorence;
+    public GenericReference<float> animationDuration;
+    public GenericReference<AnimationCurve> easingFunction;
+
+    [System.NonSerialized]
+    protected bool debounce;
+    [System.NonSerialized]
+    protected GameObject lerpTarget;
+    [System.NonSerialized]
+    protected Vector3 startVector;
+    [System.NonSerialized]
+    protected float timeSinceExecuted;
+
+    public override void Execute(GameObject callingObject)
+    {
+        if (debounce) return;
+
+        debounce = true;
+        timeSinceExecuted = 0;
+
+        lerpTarget = Instantiate(lerpObject, callingObject.transform);
+        lerpTarget.transform.position = pathToInstantiatePosition.GetValue().GetObjectAtPath(callingObject).transform.position;
+
+
+        SetUpStartVector();
+    }
+
+    protected virtual void SetUpStartVector()
+    {
+        startVector = lerpTarget.transform.position;
+    }
+
+    public override void FixedUpdateLoop(GameObject callingObject)
+    {
+        if (!debounce) return;
+
+        timeSinceExecuted += Time.deltaTime * currentSpeed.GetValue();
+
+        if (LerpToEnd())
+        {
+            if (onLerpEnd != null) onLerpEnd.Invoke(callingObject);
+
+            Destroy(lerpTarget);
+
+            debounce = false;
+        }
+    }
+
+    protected bool LerpToEnd()
+    {
+        LerpProperty(startVector, endVector.GetValue());
+
+        bool underMaximum = timeSinceExecuted < animationDuration.GetValue() + tolorence.GetValue();
+        bool overMinimum = timeSinceExecuted > animationDuration.GetValue() - tolorence.GetValue();
+
+        bool overMaximum = timeSinceExecuted > animationDuration.GetValue() + tolorence.GetValue();
+
+        if (underMaximum && overMinimum || overMaximum) return true;
+
+        return false;
+    }
+
+    protected virtual void LerpProperty(Vector3 startPosition, Vector3 endPosition)
+    {
+        lerpTarget.transform.position = Vector3.LerpUnclamped(startPosition, endPosition, easingFunction.GetValue().Evaluate(timeSinceExecuted));
+    }
+}
--- Assets/Scripts/Entities/Actions/ShootAction.cs
+++ Assets/Scripts/Entities/Actions/ShootAction.cs
@@ -10,21 +10,49 @@ public class ShootAction : ActionBase
     public GenericReference<LayerMask> layerToHit;
     public GenericReference<string> firePointName;
 
+    public GenericReference<Vector3> outputPosition;
+    public GenericReference<float> bulletSpread;
+
     [System.NonSerialized]
     Transform firePoint;
+    [System.NonSerialized]
+    bool didTracer;
+
+    [System.NonSerialized]
+    Vector3 bulletDeviation;
+    [System.NonSerialized]
+    Vector3 endPoint;
+
 
     public override void Execute(GameObject callingObject)
     {
-        if (firePoint == null) firePoint = callingObject.transform.Find(firePointName.GetValue());
+        didTracer = false;
+
+        if (firePoint == null) 
+        {
+            firePoint = callingObject.transform.Find(firePointName.GetValue());
+        }
+        
+
+        bulletDeviation.x = Random.Range(-bulletSpread.GetValue(), bulletSpread.GetValue());
+        bulletDeviation.y = Random.Range(-bulletSpread.GetValue(), bulletSpread.GetValue());
+        bulletDeviation.z = Random.Range(-bulletSpread.GetValue(), bulletSpread.GetValue());
+
+        endPoint = (bulletDeviation + firePoint.forward).normalized;
 
         RaycastHit hit;
 
-        if (Physics.Raycast(firePoint.position, firePoint.forward, out hit, maxRange.GetValue(), layerToHit.GetValue()))
+        if (Physics.Raycast(firePoint.position, endPoint, out hit, maxRange.GetValue(), layerToHit.GetValue()))
         {
             if ((hit.collider.gameObject.transform.position - firePoint.transform.position).magnitude <= maxRange.GetValue())
             {
                 Debug.Log("Dealt " + damage.GetValue() + " damage to " + hit.transform.gameObject.name);
             }
+
+            outputPosition.SetValue(hit.point);
+            didTracer = true;
         }
+
+        if (!didTracer) outputPosition.SetValue(endPoint * 1000);
     }
 }
